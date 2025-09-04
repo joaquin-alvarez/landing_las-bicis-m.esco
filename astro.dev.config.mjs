9d5ac1e486b2801d891978b28a19c41fc16e7f69
L15451 <alvarezjoaquine@gmail.com>
2025-09-04 13:40:47 +0000

    feat: add app store links and update deployment config for production

--- astro.dev.config.mjs
+++ astro.dev.config.mjs
@@ -4,8 +4,8 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     output: 'static',
-    site: 'https://www.escobar.gob.ar',
-    base: '/las-bicis',
+    site: 'https://joaquin-alvarez.github.io',
+    base: '/landing_las-bicis-m.esco',
     build: {
         assets: '_astro',
         inlineStylesheets: 'auto'
